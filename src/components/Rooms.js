b5391ba86623c9ca11c0252335eb13ac01850667
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-06-20 15:15:40 -0300

    Loading screen - finished

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -5,13 +5,19 @@ import { RoomContext } from '../context/RoomContext'
 
 // components
 import Room from '../components/Room'
+import {SpinnerDotted} from 'spinners-react'
 
 const Rooms = () => {
 
-  const { rooms } = useContext(RoomContext)
+  const { rooms, loading } = useContext(RoomContext)
 
   return (
     <section className='py-24'>
+      {loading && (
+        <div className='h-screen fixed bottom-0 top-0 right-0 bg-black/90 w-full z-50 flex items-center justify-center'>
+          <SpinnerDotted color='white'/>
+        </div>
+      )}
       <div className='container mx-auto lg:px-0'>
         <div className='grid grid-cols max-w-sm mx-auto gap-[30px] lg:grid-cols-3 lg:max-w-none lg:mx-0'>
           {rooms.map((room) => {
--- src/context/RoomContext.js
+++ src/context/RoomContext.js
@@ -11,6 +11,7 @@ const RoomProvider = ({ children }) => {
   const [adults, setAdults] = useState('1 Adulto')
   const [kids, setKids] = useState('0 Crianças')
   const [total, setTotal] = useState(0)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     setTotal(Number(adults[0]) + Number(kids[0]))
@@ -18,16 +19,20 @@ const RoomProvider = ({ children }) => {
 
   const handleClick = (e) => {
     e.preventDefault()
+    setLoading(true)
 
     // lógica de procurar quartos pelo total de pessoas que cabem nele
     const newRooms = roomData.filter((room) => {
       return total <= room.maxPerson
     })
-    setRooms(newRooms)
+    setTimeout(() => {
+      setRooms(newRooms)
+      setLoading(false)
+    }, 3000);
   }
-  
+
   return (
-    <RoomContext.Provider value={{ rooms, adults, setAdults, kids, setKids, handleClick }}>
+    <RoomContext.Provider value={{ rooms, adults, setAdults, kids, setKids, handleClick, loading }}>
       {children}
     </RoomContext.Provider>
   )
